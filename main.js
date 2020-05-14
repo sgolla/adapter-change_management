e9d56932734e19a97c139e9dc98cf6f27bf3bfad
Bang@Apr20 <santhoshrvcs@gmail.com>
2020-05-14 15:18:06 +0000

    Lab 6

--- main.js
+++ main.js
@@ -84,52 +84,20 @@ class ServiceNowAdapter extends EventEmitter {
   }
 
   /**
- * @memberof ServiceNowAdapter
- * @method healthcheck
- * @summary Check ServiceNow Health
- * @description Verifies external system is available and healthy.
- *   Calls method emitOnline if external system is available.
- *
- * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
- *   that handles the response.
- */
-healthcheck(callback) {
- this.getRecord((result, error) => {
-   /**
-    * For this lab, complete the if else conditional
-    * statements that check if an error exists
-    * or the instance was hibernating. You must write
-    * the blocks for each branch.
-    */
-   if (error) {
-     /**
-      * Write this block.
-      * If an error was returned, we need to emit OFFLINE.
-      * Log the returned error using IAP's global log object
-      * at an error severity. In the log message, record
-      * this.id so an administrator will know which ServiceNow
-      * adapter instance wrote the log message in case more
-      * than one instance is configured.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing the error seen as an argument
-      * for the callback's errorMessage parameter.
-      */
-      this.emitOffline()
-   } else {
-     /**
-      * Write this block.
-      * If no runtime problems were detected, emit ONLINE.
-      * Log an appropriate message using IAP's global log object
-      * at a debug severity.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing this function's result
-      * parameter as an argument for the callback function's
-      * responseData parameter.
-      */
-      this.emitOnline()
-   }
- });
-}
+   * @memberof ServiceNowAdapter
+   * @method healthcheck
+   * @summary Check ServiceNow Health
+   * @description Verifies external system is available and healthy.
+   *   Calls method emitOnline if external system is available.
+   *
+   * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+   *   that handles the response.
+   */
+  healthcheck(callback) {
+    // We will build this method in a later lab. For now, it will emulate
+    // a healthy integration by emmitting ONLINE.
+    this.emitOnline();
+  }
 
   /**
    * @memberof ServiceNowAdapter
@@ -184,12 +152,13 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-      this.connector.get( (data, error) => {
+
+     this.connector.get( (data, error) => {
       if (error) {
         console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
       }
       console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
-     });
+    });
   }
 
   /**
@@ -197,7 +166,7 @@ healthcheck(callback) {
    * @method postRecord
    * @summary Create ServiceNow Record
    * @description Creates a record in ServiceNow.
-   *
+   // *
    * @param {ServiceNowAdapter~requestCallback} callback - The callback that
    *   handles the response.
    */
@@ -208,6 +177,7 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
+
      this.connector.post({ serviceNowTable: 'change_request' }, (data, error) => {
       if (error) {
         console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
